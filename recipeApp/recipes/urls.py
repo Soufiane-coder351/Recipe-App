b1019925d264f1f2a96feda2b282c6edef9fd260
El Mazlouzi Soufien <soufien.elmazlouzi@student-cs.fr>
2024-11-18 10:25:36 +0100

    created the templates of our project , and the html files for that, and created some temporary views to test it

--- recipeApp/recipes/urls.py
+++ recipeApp/recipes/urls.py
@@ -1,2 +1,10 @@
+import recipes.views 
+from django.urls import path
 
-urlpatterns=[]
+urlpatterns=[
+    path('',recipes.views.index),
+    path('login/',recipes.views.login),
+    path('signup/',recipes.views.signup),
+    path('user/',recipes.views.user),
+    path('createrecipe',recipes.views.createrecipe),
+]
--- recipeApp/recipes/views.py
+++ recipeApp/recipes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template("./recipes/index.html")
+    return HttpResponse(template.render(request=request))
+
+def login(request):
+    template = loader.get_template("./recipes/login.html")
+    return HttpResponse(template.render(request=request))
+
+def signup(request):
+    template = loader.get_template("./recipes/signup.html")
+    return HttpResponse(template.render(request=request))
+
+def user(request):
+    template = loader.get_template("./recipes/user.html")
+    return HttpResponse(template.render(request=request))
+
+def createrecipe(request):
+    template = loader.get_template("./recipes/createrecipe.html")
+    return HttpResponse(template.render(request=request))
