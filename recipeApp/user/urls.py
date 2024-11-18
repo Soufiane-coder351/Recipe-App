28b70711ec82e0d29ab534fd6571a31ed0bf2573
El Mazlouzi Soufien <soufien.elmazlouzi@student-cs.fr>
2024-11-18 15:19:02 +0100

    CREATED THE AUTH, solved ton of bugs, fixed the html templates and the auth views

--- recipeApp/user/urls.py
+++ recipeApp/user/urls.py
@@ -3,7 +3,9 @@ from django.urls import re_path,path
 
 
 urlpatterns = [
-    path('login/',user.views.login, name='login'),
-    path('signup/',user.views.signup , name='signup'),
-    path('profile/',user.views.profile, name="profile"),
+    path('login/',user.views.login_view, name='login'),
+    path('signup/',user.views.signup_view , name='signup'),
+    path('profile/',user.views.profile_view, name="profile"),
+    path('logout/', user.views.logout_view, name='logout'),
+
 ]
