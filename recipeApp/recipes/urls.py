88e9060e41a199934e0910e9ccad1df86445df40
El Mazlouzi Soufien <soufien.elmazlouzi@student-cs.fr>
2024-11-19 14:58:27 +0100

    added the recipe info page, still static

--- recipeApp/recipes/urls.py
+++ recipeApp/recipes/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns=[
     path('',recipes.views.index,name='recipes'),
     path('createrecipe',recipes.views.createrecipe, name="createrecipe"),
+    path('recette-info',recipes.views.recette_info, name="recette_info"),
 ]
