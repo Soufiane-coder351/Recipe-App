320815a3227f64831bd22ed867b5b912238515d2
Ayoub Chikri <ayoub.chikri@student-cs.fr>
2024-11-19 16:00:22 +0100

    changement

--- recipeApp/recipes/models.py
+++ recipeApp/recipes/models.py
@@ -11,15 +11,6 @@ class Produit(models.Model):
     def __str__(self):
         return self.name
 
-# Modèle pour l'utilisateur (User)
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username
 
 # Modèle pour la recette (Recipe)
 class Recipe(models.Model):
