99ba3da56606c2f326a16198d5fc292e90b9af61
El Mahdaouy Asmae <asmae.elmahdaouy@student-cs.fr>
2024-11-19 10:59:01 +0100

    search

--- recipeApp/recipes/urls.py
+++ recipeApp/recipes/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns=[
     path('',recipes.views.index,name='recipes'),
     path('createrecipe',recipes.views.createrecipe, name="createrecipe"),
+    path('search', recipes.views.search, nam='search')
 ]
--- recipeApp/recipes/views.py
+++ recipeApp/recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import HttpResponse
+from recipes import models
 # Create your views here.
 def index(request):
     template = loader.get_template("./recipes/index.html")
@@ -10,3 +11,15 @@ def index(request):
 def createrecipe(request):
     template = loader.get_template("./recipes/createrecipe.html")
     return HttpResponse(template.render(request=request))
+
+def search(request):
+    query = request.GET.get('q')
+    if query:
+        results = models.Recipe.objects.filter(title__icontains=query)
+    else:
+        results = models.Recipe.objects.none()  
+
+    return render(request, 'search.html', {'resultats': results, 'query': query})
+    
+    
+        
