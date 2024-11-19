32f61ba4b398bfdf9afeabc722c481173ad37358
Ayoub Chikri <ayoub.chikri@student-cs.fr>
2024-11-19 15:55:17 +0100

    J'ai modifié la structure de User sur mon fichier local

--- recipeApp/recipes/models.py
+++ recipeApp/recipes/models.py
@@ -1,11 +1,8 @@
 
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-
-
-from django.db import models
 
 # Modèle pour l'ingrédient
 class Produit(models.Model):
