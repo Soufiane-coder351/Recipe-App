eb0c3f0c218786bc4db913ae603a4f71c0050429
El Mazlouzi Soufien <soufien.elmazlouzi@student-cs.fr>
2024-11-20 08:46:25 +0100

    basic form of creating recipesé

--- recipeApp/recipes/views.py
+++ recipeApp/recipes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from recipes.models import Recipe
 from recipes.models import Favorites
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 
 def index(request):
     template = loader.get_template("./recipes/index.html")
@@ -13,6 +13,38 @@ def index(request):
 
 @login_required
 def createrecipe(request):
+    '''
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    instructions = models.TextField()
+    '''
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        instructions = request.POST.get('instructions')
+        user = request.user
+        
+        errors = []
+        
+        if Recipe.objects.filter(title=title).exists():
+            errors.append("Titre de la recette existe déjà")
+        
+        if not errors:
+            try:
+                recipe = Recipe.objects.create(title=title,description=description,instructions=instructions,user=user)
+                recipe.save()
+                return redirect('profile')
+
+            except Exception as e:
+                errors.append(str(e))
+        
+        return render(request,'recipes/createrecipe.html', {'errors':errors, 'values' : {
+            'title':title,
+            'description':description,
+            'instructions': instructions
+        }})
+
     return render(request,"recipes/createrecipe.html")
     
 
