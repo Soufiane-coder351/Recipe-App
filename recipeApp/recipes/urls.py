1608a684fc3f641e4ce45134b85aba72aedd9760
Abitbol Solal <solal.abitbol@student-cs.fr>
2024-11-20 09:51:36 +0000

    Update urls.py, same here, changed some leftovers from the latest merge request

--- recipeApp/recipes/urls.py
+++ recipeApp/recipes/urls.py
@@ -5,13 +5,8 @@ from django.urls import path
 urlpatterns=[
     path('',recipes.views.index,name='recipes'),
     path('createrecipe',recipes.views.createrecipe, name="createrecipe"),
-<<<<<<< recipeApp/recipes/urls.py
     path('recette-info',recipes.views.recette_info, name="recette_info"),
     path('favoris/',recipes.views.afficher_favoris, name="recettes_favorites"),
     path('<str:title>',recipes.views.recette_info, name="recette_info"),
     path('chercher_recette/', recipes.views.chercher_recette, name='chercher_recette'),
-
-=======
-
->>>>>>> recipeApp/recipes/urls.py
 ]
